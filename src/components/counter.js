a141838d11c8ca330a8e3a4633ccd24a8d66b093
aliijaz1997 <57563582+aliijaz1997@users.noreply.github.com>
2020-09-22 01:31:21 +0500

    Removed useState and used useReducer instead

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 function Counter() {
-    const [counter, setCounter] = useState(0);
+    // const [counter, setCounter] = useState(0);
     const [input, setInput] = useState(0);
     const dispatch = useDispatch();
+    const counter = useSelector((state) => state.counter)
     return (
         <div >
             value of the counter is : {counter}
             <br />
             <button onClick={(() => {
-                setCounter(counter + 1)
+                // setCounter(counter + 1)
                 dispatch({type: "INCREMENT"})
             })}>Add</button>
 
             <button onClick={(() => {
-                setCounter(counter - 1)
+                // setCounter(counter - 1)
                 dispatch({type: "DECREMENT"})
             })}>Subtract</button>
 
@@ -23,7 +24,7 @@ function Counter() {
             })} />
 
             <button onClick={(() => {
-                setCounter(counter + Number(input))
+                // setCounter(counter + Number(input))
                 dispatch({type: "FORM", payload: Number(input)})
             })}>Submit</button>
         </div>
